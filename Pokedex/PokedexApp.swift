7cecfb00c50d14451160b0b17958432d4f916d59
ronyaburaihan <ronyaburaihan@gmail.com>
2023-07-14 15:50:25 +0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Abu Raihan Rony on 14/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
